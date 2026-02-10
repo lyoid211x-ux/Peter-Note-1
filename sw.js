112fa4e375f06dd187624fa335ff990079d31a2a
lyoid211x-ux <lyoid211x@gmail.com>
2026-02-10 12:54:12 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "peter-note-cache-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
