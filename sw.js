9448244fcb7e60f4cdc615bb7fd9763a71814afd
lyoid211x-ux <lyoid211x@gmail.com>
2026-02-11 06:21:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,19 @@
-const CACHE = "peter-note-cache-v1";
+const CACHE = "peter-note-cache-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
+self.addEventListener("activate", (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
